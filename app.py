7410a1aa22115f0e0712a07646911c9ffa211188
bronzefloo <isabellamartins@gmail.com>
2025-03-26 19:19:21 -0300

    requirements.txt e configuração CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
 import sqlite3
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 def init_db():
